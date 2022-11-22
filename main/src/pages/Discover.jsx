7c94339e89425cd8ddea61e93f1c13a4bc6895d8
Michal <78083264+TenMichal@users.noreply.github.com>
2022-11-22 16:41:07 +0100

    base

---
+++ main/src/pages/Discover.jsx
@@ -0,0 +1,11 @@
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+
+const Discover = () => {
+    console.log('res')
+    return (
+        <div>Discover</div>
+    );
+};
+
+export default Discover;
