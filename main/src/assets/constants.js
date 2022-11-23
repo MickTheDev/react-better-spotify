267ba30ee1bd9fcf28cbc490b2a721e3a0c89797
Michal <78083264+TenMichal@users.noreply.github.com>
2022-11-23 18:49:31 +0100

    sidebar section

--- main/src/assets/constants.js
+++ main/src/assets/constants.js
@@ -1,25 +1,30 @@
-import { HiOutlineHashtag, HiOutlineHome, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
+import {
+	HiOutlineHashtag,
+	HiOutlineHome,
+	HiOutlinePhotograph,
+	HiOutlineUserGroup,
+} from 'react-icons/hi';
 
 export const genres = [
-  { title: 'Pop', value: 'POP' },
-  { title: 'Hip-Hop', value: 'HIP_HOP_RAP' },
-  { title: 'Dance', value: 'DANCE' },
-  { title: 'Electronic', value: 'ELECTRONIC' },
-  { title: 'Soul', value: 'SOUL_RNB' },
-  { title: 'Alternative', value: 'ALTERNATIVE' },
-  { title: 'Rock', value: 'ROCK' },
-  { title: 'Latin', value: 'LATIN' },
-  { title: 'Film', value: 'FILM_TV' },
-  { title: 'Country', value: 'COUNTRY' },
-  { title: 'Worldwide', value: 'WORLDWIDE' },
-  { title: 'Reggae', value: 'REGGAE_DANCE_HALL' },
-  { title: 'House', value: 'HOUSE' },
-  { title: 'K-Pop', value: 'K_POP' },
+	{ title: 'Pop', value: 'POP', id: 1 },
+	{ title: 'Hip-Hop', value: 'HIP_HOP_RAP', id: 2 },
+	{ title: 'Dance', value: 'DANCE', id: 3 },
+	{ title: 'Electronic', value: 'ELECTRONIC', id: 4 },
+	{ title: 'Soul', value: 'SOUL_RNB', id: 5 },
+	{ title: 'Alternative', value: 'ALTERNATIVE', id: 6 },
+	{ title: 'Rock', value: 'ROCK', id: 7 },
+	{ title: 'Latin', value: 'LATIN', id: 8 },
+	{ title: 'Film', value: 'FILM_TV', id: 9 },
+	{ title: 'Country', value: 'COUNTRY', id: 10 },
+	{ title: 'Worldwide', value: 'WORLDWIDE', id: 11 },
+	{ title: 'Reggae', value: 'REGGAE_DANCE_HALL', id: 12 },
+	{ title: 'House', value: 'HOUSE', id: 13 },
+	{ title: 'K-Pop', value: 'K_POP', id: 14 },
 ];
 
 export const links = [
-  { name: 'Discover', to: '/', icon: HiOutlineHome },
-  { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
-  { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
-  { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
+	{ name: 'Discover', to: '/', icon: HiOutlineHome },
+	{ name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
+	{ name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
+	{ name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
 ];
--- main/src/index.jsx
+++ main/src/index.jsx
@@ -8,11 +8,9 @@ import App from './App';
 import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-  </React.StrictMode>,
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>
 );
